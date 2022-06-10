99d9d79bc1d4d65bc163b07d495f9034c85ed87c
Miss-zhao-source <884181586@qq.com>
2022-06-10 22:47:21 +0800

    使用装饰器parametrize测试用户列表

--- test_1_3_1_user_list.py
+++ test_1_3_1_user_list.py
@@ -1,16 +1,17 @@
 import logging
 import requests
+import pytest
 
+user_list=[
+    ["",1,20,"获取管理员列表成功"],
+    ["","",20, "pagenum 参数错误"],
+    ["",1,"","pagesize 参数错误"]
+]
 class Test_1_3_1_user_list:
     def setup_class(self):
         self.url='http://127.0.0.1:8888/api/private/v1/users'
         logging.info("self.url==============={}".format(self.url))
         self.flag = 0
-        self.data = [
-            {"query":"","pagenum":1,"pagesize":20,"msg":"获取管理员列表成功"},
-            {"query": "", "pagenum": '', "pagesize": 20, "msg": "pagenum 参数错误"},
-            {"query": "", "pagenum": '1', "pagesize": '', "msg": "pagesize 参数错误"}
-        ]
 
     def teardown_class(self):
         print('Print CLASS END')
@@ -24,20 +25,14 @@ class Test_1_3_1_user_list:
     def teardown(self):
         print('test user list end')
 
-    def test_user_list(self):
-        for i in range(len(self.data)):
-            data_test = self.data[i]
-            data = {
-                "query":data_test["query"],
-                "pagenum":data_test["pagenum"],
-                "pagesize":data_test["pagesize"],
-            }
 
-            url_str='?query={}&pagenum={}&pagesize={}'.format(data['query'],data['pagenum'],data['pagesize'])
-            r = requests.get(url=self.url+url_str, headers=self.headers, data=data)
-            logging.info(r.json())
-            logging.info(r.status_code)
-            assert r.json()['meta']['msg']==data_test['msg']
+    @pytest.mark.parametrize('query,pagenum,pagesize,msg',user_list)
+    def test_user_list(self,query,pagenum,pagesize,msg):
+        url_str='?query={}&pagenum={}&pagesize={}'.format(query,pagenum,pagesize)
+        r = requests.get(url=self.url+url_str, headers=self.headers)
+        logging.info(r.json())
+        logging.info(r.status_code)
+        assert r.json()['meta']['msg']==msg
 
 
 
